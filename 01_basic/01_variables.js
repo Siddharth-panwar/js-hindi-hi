9363e2162b71feaa75156a2cf414f926264364b7
Siddharth-panwar <siddharthpanwar299@gmail.com>
2024-10-06 04:52:54 +0000

    done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 122323
+let accountEmail = "Siddharth@google.com"
+var accountPassword = "12234"
+accountCity = "Uttarakhand"
+
+// accountId = 2 // not allowed 
+
+accountEmail = "sidd@hc.com"
+accountPassword = "121212"
+accountCity = "Jaipur"
+
+console.log(accountId);
+console.table([ accountId, accountEmail, accountPassword, accountCity])
